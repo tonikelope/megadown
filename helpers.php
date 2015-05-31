3c9364486d373e9de5d31fd78ffe6344e25ab8b5
tonikelope <tonikelope@gmail.com>
2015-05-31 18:10:17 +0200

    Password links update

--- helpers.php
+++ helpers.php
@@ -15,15 +15,15 @@ function jsonParam($json, $var)
 
 function passwordCheck($password, $mc_pass)
 {
-	list($iter_log2, $key_check, $salt) = explode('#', $mc_pass);
+	list($iter_log2, $key_check, $salt, $iv) = explode('#', $mc_pass);
 
 	$hmac = passwordHMAC('sha256', $password, base64_decode($salt), pow(2, $iter_log2));
 
-	if (hash('sha256', $hmac, true) !== base64_decode($key_check)) {
+	if (hash_hmac('sha256', $hmac, base64_decode($iv), true) !== base64_decode($key_check)) {
 		return 'bad-password';
 	}
 
-	return bin2hex($hmac);
+	return bin2hex($hmac).'#'.bin2hex(base64_decode($iv));
 }
 
 function passwordHMAC($algo, $pass, $salt, $iterations)
