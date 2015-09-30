944e8df546142d85b726e30a66d83ec7c507e5e2
tonikelope <tonikelope@gmail.com>
2015-09-30 19:01:12 +0200

    /#N! , mega://enc? and mega://enc2? links support

--- helpers.php
+++ helpers.php
@@ -37,4 +37,36 @@ function passwordHMAC($algo, $salt, $pass, $iterations)
 	return $xor;
 }
 
+function aesCbcDecrypt($data, $key, $iv = null, $pkcs7pad = false) {
+        $dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, is_null($iv) ? pack('x' . mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC)) : $iv);
+
+        return $pkcs7pad ? pkcs7UnPad($dec) : $dec;
+    }
+    
+function pkcs7UnPad($data) {
+        $pad = ord($data[strlen($data) - 1]);
+
+        return ($pad > strlen($data) || strspn($data, chr($pad), strlen($data) - $pad) != $pad) ? false : substr($data, 0, -1 * $pad);
+    }
+
+function urlBase64Decode($data) {
+        return base64_decode(str_replace(['-', '_', ','], ['+', '/', ''], str_pad($data, strlen($data) + (4 - strlen($data) % 4) % 4, '=')));
+    }
+    
+function decryptMegaDownloaderLinks($data) {
+        
+        return preg_replace_callback('/mega\:\/\/(?P<folder>f)?(?P<enc>enc\d*?)\?(?P<linkdata>[\da-z_,-]*?)(?=https?\:|mega\:|[^\da-z_,-]|$)/i', 
+                
+            function($match) {
+				
+                $key = ['enc' => '6B316F36416C2D316B7A3F217A30357958585858585858585858585858585858', 
+						'enc2' => 'ED1F4C200B35139806B260563B3D3876F011B4750F3A1A4A5EFD0BBE67554B44'];
+
+                $iv = '79F10A01844A0B27FF5B2D4E0ED3163E';
+
+                return 'https://mega.nz/#' . strtoupper($match['folder']) . aesCbcDecrypt(urlBase64Decode($match['linkdata']), hex2bin($key[$match['enc']]), hex2bin($iv), true); }, 
+                
+            $data);
+    }
+    
 echo call_user_func_array($argv[1], array_slice($argv, 2));
